9ac8af996eaa944ceee4a85846a7b822fa481cd6
Long <yangfeilong@moseeker.com>
2020-11-03 20:09:31 +0800

    20201103 广度优先搜索

--- src/main/java/niuke/剑指offer/二叉树/jz32.java
+++ src/main/java/niuke/剑指offer/二叉树/jz32.java
@@ -1,10 +1,64 @@
 package niuke.剑指offer.二叉树;
 
+import niuke.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class jz32 {
     public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(3);
+        levelOrder2(treeNode);
 
 
+    }
 
+    /**
+     * 从上到下打印二叉树（广度优先搜索）
+     * @param root
+     * @return
+     */
+    public static int[] levelOrder2(TreeNode root) {
+        if (root == null) return new int[0];
+        //queue简便写法
+        Queue<TreeNode> queue = new LinkedList(){{add(root);}};
+        /**
+         * 队列使用，offer优于add（不能添加返回false）
+         * poll优于remove（为空时返回null而不是错误）
+         * peek优于element(查询元素)
+         */
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            arrayList.add(node.val);
+            if(root.left != null) queue.offer(root.left);
+            if(root.right != null) queue.offer(root.right);
+
+        }
+        int[] abs = new int[arrayList.size()];
+        for (int i=0;i<arrayList.size();i++){
+            abs[i] = arrayList.get(i);
+        }
+        return abs;
+    }
+
+    public int[] levelOrder(TreeNode root) {
+        if(root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList(){{add(root);}};
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if(root.left != null) queue.offer(node.left);
+            if(root.right != null) queue.offer(node.right);
+
+        }
+        int[] abs = new int[ans.size()];
+        for (int i=0;i<ans.size();i++){
+            abs[i] = ans.get(i);
+        }
+        return abs;
     }
 
 }
