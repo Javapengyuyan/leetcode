c072c0879592b0f00b65b3ce4a9a7726ad14c1a9
long <1249586737@qq.com>
2021-01-04 21:54:45 +0800

    2021 0104 回溯

---
+++ src/main/java/algorithm/回溯/CheckWord.java
@@ -0,0 +1,27 @@
+package algorithm.回溯;
+
+import java.util.Scanner;
+
+public class CheckWord {
+
+    public static void main(String[] args) {
+        //给定二位数组
+        int[][] root = new int[][]{};
+        //int x = root[0].length;
+        int y = root.length;
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        String[] strings = word.split("");
+        //todo 递归判断
+
+
+
+
+
+    }
+
+
+
+
+
+}
