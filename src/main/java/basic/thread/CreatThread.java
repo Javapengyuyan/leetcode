7a7c6a2f0c0d8c61ac465b0ac455d252bcd6b8fe
long <1249586737@qq.com>
2021-01-24 22:05:58 +0800

    20210124 多线程

---
+++ src/main/java/basic/thread/CreatThread.java
@@ -0,0 +1,21 @@
+package basic.thread;
+
+public class CreatThread extends Thread {
+
+    @Override
+    public void run(){
+
+    }
+
+    //thread run 和 start的差异
+
+    //同时下载三个图片
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
