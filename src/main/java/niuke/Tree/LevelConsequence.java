7ca39b4c43cf8422c12db5d8f49a1ff2ee727638
long <1249586737@qq.com>
2021-04-22 09:25:09 +0800

    20210422 二叉树层序遍历

---
+++ src/main/java/niuke/Tree/LevelConsequence.java
@@ -0,0 +1,21 @@
+package niuke.Tree;
+
+import niuke.common.TreeNode;
+
+import java.util.ArrayList;
+
+public class LevelConsequence {
+
+    public static void main(String[] args) {
+        TreeNode root;
+
+    }
+
+
+    public static ArrayList<ArrayList<Integer>> node(TreeNode root){
+        return null;
+
+    }
+
+
+}
