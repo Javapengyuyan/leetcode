3d00a0137c47cbf5dde86c9071b03b2f134471cf
git <git>
2021-04-23 17:53:25 +0800

    20210423 二叉树层级遍历

--- src/main/java/niuke/Tree/LevelConsequence.java
+++ src/main/java/niuke/Tree/LevelConsequence.java
@@ -2,19 +2,65 @@ package niuke.Tree;
 
 import niuke.common.TreeNode;
 
-import java.util.ArrayList;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class LevelConsequence {
 
-    public static void main(String[] args) {
-        TreeNode root;
+    /**
+     * 二叉树层级遍历
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        /*Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();*/
+        TreeNode root = new TreeNode(9);
+        node(root);
+
+
 
     }
 
 
-    public static ArrayList<ArrayList<Integer>> node(TreeNode root){
-        return null;
 
+    public static ArrayList<ArrayList<Integer>> node(TreeNode root){
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        ArrayList<ArrayList<Integer>> queue = new ArrayList<>();
+        LinkedList<TreeNode> treeNodes = new LinkedList<>();
+        treeNodes.add(root);
+        while (!treeNodes.isEmpty()){
+            ArrayList nodeList = new ArrayList();
+            Integer size = treeNodes.size();
+            for (;size>0;size--){
+                TreeNode node = treeNodes.pollFirst();
+                if (node.left != null){
+                    treeNodes.addLast(node.left);
+                }
+                if (node.right != null){
+                    treeNodes.addLast(node.right);
+                }
+                nodeList.add(node.val);
+            }
+            queue.add(nodeList);
+
+        }
+        return queue;
+    }
+
+    /**
+     *  获取 HashMap 实际容量大小
+     * @param hashMap
+     * @return
+     * @throws Exception
+     */
+    public static Integer getCapacity(HashMap hashMap) throws Exception {
+        Method capacity = hashMap.getClass().getDeclaredMethod("capacity");
+        capacity.setAccessible(true);
+        Integer realCapacity = (Integer) capacity.invoke(hashMap);
+        return realCapacity;
     }
 
 
