97de741bc5c6992c9df36842583c5a387fad2dc6
git <git>
2021-06-01 15:24:59 +0800

    20210601 并发编程--可见性

---
+++ src/main/java/ConcureentProgramming/ChapterThree/Novisibility.java
@@ -0,0 +1,28 @@
+package ConcureentProgramming.ChapterThree;
+
+public class Novisibility {
+
+    private  static  boolean ready;
+    private static  int number;
+
+    private static class ReaderThread extends Thread{
+        @Override
+        public void run(){
+            while ( !ready ){
+                Thread.yield();
+                System.out.println(number);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().run();
+        number = 42;
+        ready = true;
+
+    }
+
+
+
+}
