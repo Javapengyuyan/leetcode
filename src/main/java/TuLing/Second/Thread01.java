8418d39490973f36bfd8475b86344bbf89ab731a
long <1249586737@qq.com>
2021-06-07 18:56:47 +0800

    20210607 Java线程KLT测试验证

---
+++ src/main/java/TuLing/Second/Thread01.java
@@ -0,0 +1,27 @@
+package TuLing.Second;
+
+public class Thread01 {
+
+    /**
+     * 线程创建KLT测试
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i=0;i<200;i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true){
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+
+                        }
+                    }
+                }
+            }).start();
+        }
+
+    }
+
+}
