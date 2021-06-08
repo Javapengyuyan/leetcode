aeb92f88ef1d6f709bdcd9e01d08341848679149
long <1249586737@qq.com>
2021-06-08 20:38:19 +0800

    20210608 Java内存模型

---
+++ src/main/java/TuLing/Second/Jmm05.java
@@ -0,0 +1,67 @@
+package TuLing.Second;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Jmm05 {
+
+    /**
+     * 可能出现指令重排，测试代码
+     */
+    private static int x = 0,y=0;
+    private static int a=0,b=0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i=0;
+        for (;;){
+            i++;
+            x=0;y=0;
+            a=0;b=0;
+            Thread t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    shortWait(1000);
+
+                    a=1;
+                    x = b;
+                }
+            });
+
+            Thread t2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    b=1;
+                    y=a;
+                }
+            });
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+
+            String result ="第" + i +"次("+x+","+y+")";
+            if (x == 0 && y==0){
+                System.out.println(result);
+                break;
+            }else {
+                log.info(result);
+            }
+
+        }
+
+
+    }
+
+    public static void shortWait(Integer time){
+        long start = System.nanoTime();
+        long end;
+        do {
+            end = System.nanoTime();
+        }while (start + time >= end);
+
+    }
+
+
+
+
+}
---
+++ src/main/java/TuLing/Second/Thread02.java
@@ -0,0 +1,47 @@
+package TuLing.Second;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Thread02 {
+
+    private static boolean initflag = false;
+    private volatile static int counter = 0;
+
+    public static void refresh(){
+        log.info("refer data ....");
+        initflag = true;
+        log.info("refer data success....");
+    }
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(()->{
+            while (! initflag){
+                counter++;
+            }
+            log.info("线程:"+Thread.currentThread().getName()+"嗅到initflag改变");
+
+        },"threadA");
+        thread.start();
+
+        try {
+            thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Thread threadB = new Thread(()->{
+            refresh();
+        },"threadB");
+        threadB.start();
+        try {
+            threadB.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
