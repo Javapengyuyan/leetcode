d0e9c5f163d3f6f4dc73dce5ab74a337fcc81fca
long <1249586737@qq.com>
2021-06-11 23:38:09 +0800

    20210611 JZ

---
+++ src/main/java/leetCode/JianZhi/JZ10.java
@@ -0,0 +1,16 @@
+package leetCode.JianZhi;
+
+public class JZ10 {
+    public static void main(String[] args) {
+        int i=1,j=1,sum =0;
+        int time = 18;
+        for (int x=0;x<time;x++){
+            sum = (i + j) % 1000000007;
+            i = j;
+            j = sum;
+        }
+        System.out.println(i);
+
+    }
+
+}
