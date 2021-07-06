b5e5305b325bb434eddbc6dd98d4d1bf0561dcce
long <1249586737@qq.com>
2021-07-06 22:45:57 +0800

    20210706 并发--变量共享

---
+++ src/main/java/Concureency/ChapterThree/NoVisibility.java
@@ -0,0 +1,50 @@
+package Concureency.ChapterThree;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 没有共享可能导致重排序，读取不到，或者读到0
+ *
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread{
+        public void run(){
+            while (!ready){
+                Thread.yield();
+            }
+            System.out.println(number);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+
+
+
+    //3.3.3
+    private static ThreadLocal<Connection> connectionThreadLocal
+            = new ThreadLocal<Connection>(){
+        public Connection initialValue(){
+            try {
+                return DriverManager.getConnection("DB_URL");
+            } catch (SQLException e) {
+                return null;
+            }
+        }
+    };
+    public static Connection getConnection(){
+        return connectionThreadLocal.get();
+    }
+
+
+
+}
