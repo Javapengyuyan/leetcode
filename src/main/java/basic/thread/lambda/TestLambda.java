042f8f40fd96cb33b642fc2470095ce4a579da7c
long <1249586737@qq.com>
2022-02-13 19:06:16 +0800

    20220213 线程停止

---
+++ src/main/java/basic/thread/lambda/TestLambda.java
@@ -0,0 +1,65 @@
+package basic.thread.lambda;
+
+/**
+ * @author: long
+ * @create: 2022-02-12 22:00
+ * @Description 函数式接口逐渐递进
+ *
+ * 特性：1、一个接口只能有一个实现方法
+ *      2、看着简洁
+ **/
+
+public class TestLambda {
+
+    //3、静态内部类
+    static class Like2 implements ILike{
+        @Override
+        public void lambda() {
+            System.out.println("3、静态内部类");
+        }
+    }
+
+    public static void main(String[] args) {
+        ILike like = new Like();
+        like.lambda();
+        like = new Like2();
+        like.lambda();
+
+        //4、局部内部类
+        class Like3 implements ILike{
+            @Override
+            public void lambda() {
+                System.out.println("4、局部内部类");
+            }
+        }
+        like = new Like3();
+        like.lambda();
+        //5、匿名内部类 必须借助接口和父类
+        like = new ILike() {
+            @Override
+            public void lambda() {
+                System.out.println("5、匿名内部类");
+            }
+        };
+        like.lambda();
+        //6、lambda简化
+        like = ()->{
+            System.out.println("6、lambda表达式");
+        };
+        like.lambda();
+
+    }
+
+
+}
+//1、定义一个函数式接口
+interface ILike{
+    void lambda();
+}
+//2、实现类
+class Like implements ILike{
+    @Override
+    public void lambda() {
+        System.out.println("1、接口实现类");
+    }
+}
---
+++ src/main/java/basic/thread/state/TestStop.java
@@ -0,0 +1,49 @@
+package basic.thread.state;
+
+/**
+ * @author: long
+ * @create: 2022-02-13 00:17
+ * @Description  测试线程停止
+ *
+ * 1、线程正常停止，--利用次数，不建议死循环
+ * 2、建议使用标识位---设置一个标识位标识是否停止
+ * 3、不要使用过期方法stop、destory等
+ *
+ **/
+
+public class TestStop implements Runnable{
+
+    //1、设置一个标识位
+    private boolean flag = true;
+
+    @Override
+    public void run() {
+        int i= 0;
+        while (flag){
+            System.out.println("run......Thread"+i++);
+        }
+    }
+
+    //2、设置一个公开方法停止线程，转换标识位
+    public void stop(){
+        this.flag = false;
+    }
+
+    public static void main(String[] args) {
+        TestStop testStop = new TestStop();
+        new Thread(testStop).start();
+        for (int i = 0; i <= 200; i++) {
+            System.out.println("main"+i);
+            if (i == 150){
+                testStop.stop();
+                System.out.println("该线程停止了");
+            }
+        }
+
+
+    }
+
+
+
+
+}
