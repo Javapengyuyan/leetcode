be5c0f0607cff0d9ccec27ec078f33627b7f77c8
long <1249586737@qq.com>
2022-05-08 16:40:07 +0800

    20220508 结构调整

--- src/main/java/com/javaBasic/javaSE/equalsTest/Student.java
+++ src/main/java/com/javaBasic/javaSE/equalsTest/Student.java
@@ -8,6 +8,16 @@ public class Student {
     private String name;
     private int age;
     private String interest;
+    private int salary;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
 
     @Override
     public boolean equals(Object obj){
